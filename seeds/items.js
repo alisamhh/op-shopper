0a3ad66745394b094333b7d6c19fa146403efe8d
Alisa Herrera Hayman <alisamhh@gmail.com>
2018-09-06 10:44:38 +1200

    Added margin

--- seeds/items.js
+++ seeds/items.js
@@ -5,7 +5,8 @@ exports.seed = function (knex, Promise) {
         {id: 22201, category: 'Clothing', subcategory: 'Tops', item: 'Jacket', size: 'Small', condition: 'Great', brand: 'The North Face', user_id: 99901},
         {id: 22202, category: 'Clothing', subcategory: 'Pants', item: 'Yoga Pants', size: 'Medium', condition: 'Good', brand: 'Express', user_id: 99902},
         {id: 22203, category: 'Appliances', subcategory: 'Kitchen', item: 'Fridge', size: 'Small', condition: 'Great', brand: 'Fisher & Paykel', user_id: 99901},
-        {id: 22204, category: 'Clothing', subcategory: 'Footwear', brand: 'Converse', item: 'Shoes', size: '8', condition: 'Good', user_id: 99902}
+        {id: 22204, category: 'Clothing', subcategory: 'Footwear', brand: 'Converse', item: 'Shoes', size: '8', condition: 'Good', user_id: 99902},
+        {id: 22205, category: 'Clothing', subcategory: 'Tops', brand: 'American Apparel', item: 'Shirt', size: 'S', condition: 'Used', user_id: 99902}
       ])
     })
 }
